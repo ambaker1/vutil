6edcc9db2196e8483e5bdb5db3059ac0b0ebfb26
Alex Baker <ambaker1@mtu.edu>
2023-04-26 14:55:09 -0500

    Updated version number (and added src files)

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -1,2 +1,2 @@
 if {![package vsatisfies [package provide Tcl] 8.6]} {return}
-package ifneeded vutil 0.1.0 [list source [file join $dir vutil.tcl]]
+package ifneeded vutil 0.1.1 [list source [file join $dir vutil.tcl]]
