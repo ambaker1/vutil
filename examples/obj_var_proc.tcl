fbd812d198d17bb245fa096d74df90c57cff7fc9
Alex Baker <48138840+ambaker1@users.noreply.github.com>
2023-08-14 19:39:58 -0500

    Updated examples

--- examples/obj_var_proc.tcl
+++ examples/obj_var_proc.tcl
@@ -2,13 +2,12 @@ package require tin
 tin import vutil
 # Example showing how object variables behave in procedures
 proc foo {value} {
-    # Create named object with reference variable "result"
-    var create myObj result $value
+    # Create object with reference variable "result"
+    var new result $value
     append $result { world}
     return [list $result [$result]]; # Returns name and value of object
 }
 set result [foo hello]; # Not the same "result"
 lassign $result name value
-puts $name; # ::myObj
 puts $value; # hello world
 puts [info object isa object $name]; # 0 (object was deleted when procedure returned)
